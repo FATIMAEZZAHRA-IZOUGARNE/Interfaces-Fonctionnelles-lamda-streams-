0838fcfcb5f27ad19e547135786a8a288e28a6bd
FATIMAEZZAHRA-IZOUGARNE <fatimaezzahraizougarne@gmail.com>
2026-01-11 22:36:31 +0100

    Add files via upload

---
+++ Operation.java
@@ -0,0 +1,13 @@
+//@FunctionalInterface
+public interface Operation {
+	double calculer(double x, double y);
+	//double add(double x, double y);
+	//double soustraction(double x, double y);
+	//double multiplication(double x, double y);
+	//double division(double x, double y);
+}
+
+
+
+
+
